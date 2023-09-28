436d82c964368fee36a9ce92cc83cb5e7f7b681b
fridapadilla21 <145025707+fridapadilla21@users.noreply.github.com>
2023-09-28 08:32:36 -0600

    Create condicionales02.py

---
+++ condicionales02.py
@@ -0,0 +1,10 @@
+numero = int(input("Dame un numero"))
+print("Vamos a ver :)")
+print(" ")
+
+if numero > -10 < 10:
+  print("Hurra!!")
+elif numero == -10:
+  print("Hurra!!")
+else:
+  print("Número invalido")
