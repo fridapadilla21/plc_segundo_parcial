b5063eb3c7ad1004c40114ad88fa498526136b89
fridapadilla21 <145025707+fridapadilla21@users.noreply.github.com>
2023-09-28 08:31:40 -0600

    Create condicionales01.py

---
+++ condicionales01.py
@@ -0,0 +1,11 @@
+numero = float(input("Dame un número: "))
+print("Veamos si es positivo, negativo o cero")
+
+if numero < 0:
+  print("Este numero es negativo")
+elif numero == 0:
+  print("Este es cero")
+elif numero > 1:
+  print("Este numero es positivo")
+else:
+  print("este numero es invalido")
